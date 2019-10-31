1eae2257702aef74766103d552da6f5c6deecf7d
scriptsandsuch <57178676+scriptsandsuch@users.noreply.github.com>
2019-10-31 15:40:10 +0200

    Update sg.sh

--- sg.sh
+++ sg.sh
@@ -10,12 +10,12 @@ else
 	echo "You have set the wrong username for the ubuntu installation, please reinstall with a user named user"
 	exit
 fi
-git clone https://github.com/scriptsandsuch/sg-script.git
 wget https://s3.eu-central-1.amazonaws.com/airgap.anyvision.co/better_environment/betterenvironment-181202-142-linux-x64-installer.run https://s3.eu-central-1.amazonaws.com/facesearch.co/installbuilder/1.20.0/FaceRec-1.20.0-66-local-gpu-linux-x64-installer.run https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 
 chmod 777 better*
 chmod 777 Face*
 apt install vlc curl vim htop net-tools git -y
+git clone https://github.com/scriptsandsuch/sg-script.git
 apt install ./team* -y
 add-apt-repository --yes --update ppa:graphics-drivers/ppa
 apt install nvidia-driver-410 -y
@@ -23,7 +23,7 @@ mkdir /home/user/moxa-config/
 mv /home/user/Downloads/sg-script/moxa_e1214.sh /home/user/moxa-config/
 mv /home/user/Downloads/sg-script/cameraList.json /home/user/moxa-config/
 chmod +x /home/user/moxa-config/*
-touch /tmp/sgflag
+touch /tmp/sg.f
 
 echo -e "\e[1m Please Rebbot your machine, then isntall the enviroment and FaceRec, after that run this script again"
  
@@ -48,7 +48,7 @@ sed -i "s|api.tls.ai|api-$host.tls.ai|g" /home/user/docker-compose/1.20.0/docker
 #cp /home/user/Downloads/sg-script/SGLogo.jpg /home/user/dashboard/images/SGLogo.jpg
 }
 
-if [[ -f "/tmp/sgflag" ]]; then
+if [[ -f "/tmp/sg.f" ]]; then
 	after_reboot
 else
 	before_reboot
