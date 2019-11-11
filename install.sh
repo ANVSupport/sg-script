1fe6c6f9c324bdb334f14334422a12d25a957679
scriptsandsuch <57178676+scriptsandsuch@users.noreply.github.com>
2019-11-11 15:28:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #! usr/bin/env bash
 before_reboot() {
-if [ "$EUID" -ne 0 ]; then	
-	echo "Please run this script as root"
-	exit
-fi
 if [[ -d "/home/user/" ]]; then
 	cd /home/user/Downloads
 else
@@ -11,26 +7,51 @@ else
 	exit
 fi
 ##Download files and install what is needed
+mount_storage ##when func is ready
+while true; do
+    read -p "Do you wish to install the enviroment automatically? [y/n]" yn
+    case $yn in
+        [Yy]* ) install_env; break;;
+        [Nn]* ) 
+				wget https://s3.eu-central-1.amazonaws.com/airgap.anyvision.co/better_environment/betterenvironment-181202-142-linux-x64-installer.run
+				exit;;
+        * ) echo "${red}Invalid Answer${reset}";;
+    esac
+dpkg -a --configure
 wget https://s3.eu-central-1.amazonaws.com/facesearch.co/installbuilder/1.20.0/FaceRec-1.20.0-66-local-gpu-linux-x64-installer.run 
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
 chmod +x Face*
-apt install vlc curl vim htop net-tools git apt-transport-https ca-certificates software-properties-common -y
+apt install vlc curl vim htop net-tools git apt-transport-https ca-certificates software-properties-common -y && SuccesfulPrint "Utilities"
 git clone https://github.com/scriptsandsuch/sg-script.git
-apt install ./team* -y
+apt install ./team* -y && SuccesfulPrint "TeamViewer"
 
-##install enviroment
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+
+##moxa set up
+moxadir = /home/user/moxa-config/
+mkdir $moxadir
+mv /home/user/Downloads/sg-script/moxa_e1214.sh $moxadir
+mv /home/user/Downloads/sg-script/cameraList.json $moxadir
+chmod +x /home/user/moxa-config/*
+
+echo "1" > /opt/sg.f ##flag if the script has been run
+echo -e "Please reboot your machine, then isntall FaceRec, uncheck \"start services\" and \"start application dashboard\" on the end of the installation after that run this script again"
+ 
+}
+
+Install_env(){
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" 
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+echo $distribution ##debug
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | tee /etc/apt/sources.list.d/nvidia-docker.list
 apt update
-sudo apt-get install docker-ce=5:18.09.7~3-0~ubuntu-bionic -y
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
+apt install docker-ce=5:18.09.7~3-0~ubuntu-bionic -y && SuccesfulPrint "docker-ce"
+curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
 add-apt-repository --yes --update ppa:graphics-drivers/ppa
-apt install nvidia-driver-410 nvidia-modprobe -y
-sudo apt-get install -y nvidia-docker2
-	sudo tee /etc/docker/daemon.json <<'EOF'
+apt install nvidia-driver-410 nvidia-modprobe -y && SuccesfulPrint "Nvidia drivers"
+apt install -y nvidia-docker2 && SuccesfulPrint "Nvidia Docker"
+tee /etc/docker/daemon.json <<'EOF' > /dev/null
 {
     "default-runtime": "nvidia",
     "runtimes": {
@@ -41,53 +62,63 @@ sudo apt-get install -y nvidia-docker2
     }
 }
 EOF
-sudo pkill -SIGHUP dockerd
-
-##moxa set up
-mkdir /home/user/moxa-config/
-mv /home/user/Downloads/sg-script/moxa_e1214.sh /home/user/moxa-config/
-mv /home/user/Downloads/sg-script/cameraList.json /home/user/moxa-config/
-chmod +x /home/user/moxa-config/*
-echo "1" > /opt/sg.f
-
-echo -e "Please reboot your machine, then isntall FaceRec, uncheck "start services" on the end of the installation after that run this script again"
- 
+echo "{green}Runtime added{reset}"
+pkill -SIGHUP dockerd
 }
 
 
-##second iteration
+
+
 after_reboot(){
 ##edit env and yml
+dockerfile = "/home/user/docker-compose/1.20.0/docker-compose.yml"
 tee -a /home/user/docker-compose/1.20.0/env/broadcaster.env <<'EOF'
 ## Modbus plugin integration
 BCAST_MODBUS_IS_ENABLED=true
 BCAST_MODBUS_CMD_PATH=/home/user/moxa-config/moxa_e1214.sh
 BCAST_MODBUS_CAMERA_LIST_PATH=/home/user/moxa-config/cameraList.json
 EOF
-##echo -e "\n## Modbus plugin integration" >> /home/user/docker-compose/1.20.0/env/broadcaster.env
-##echo 'BCAST_MODBUS_IS_ENABLED=true' >> /home/user/docker-compose/1.20.0/env/broadcaster.env
-##echo 'BCAST_MODBUS_CMD_PATH=/home/user/moxa-config/moxa_e1214.sh' >> /home/user/docker-compose/1.20.0/env/broadcaster.env
-##echo 'BCAST_MODBUS_CAMERA_LIST_PATH=/home/user/moxa-config/cameraList.json' >> /home/user/docker-compose/1.20.0/env/broadcaster.env
-line=$(grep -nF broadcaster.tls.ai /home/user/docker-compose/1.20.0/docker-compose.yml  | awk -F: '{print $1}')
-pos=$((line+2))
+line=$(grep -nF broadcaster.tls.ai /home/user/docker-compose/1.20.0/docker-compose.yml  | awk -F: '{print $1}') ; line=$((line+2))
 host=$(hostname)
-sed -i "${pos}i \      - \/home\/user\/moxa-config:\/home\/user\/moxa-config" /home/user/docker-compose/1.20.0/docker-compose.yml 
-sed -i "s|nginx-\${node_name:-localnode}.tls.ai|nginx-$host.tls.ai|g" /home/user/docker-compose/1.20.0/docker-compose.yml
-sed -i "s|api.tls.ai|api-$host.tls.ai|g" /home/user/docker-compose/1.20.0/docker-compose.yml
-
-##finalize setup
-cd /home/user/docker-compose/1.20.0/
-docker-compose up -d
-footprint=$(docker exec -it $(docker ps | grep backend | awk '{print $1}') license-ver -o)
-echo "your footprint is:"
-echo $footprint
-echo "2" > /opt/sg.f
+sed -i "${line}i \      - \/home\/user\/moxa-config:\/home\/user\/moxa-config" $dockerfile
+sed -i "s|nginx-\${node_name:-localnode}.tls.ai|nginx-$host.tls.ai|g" $dockerfile
+sed -i "s|api.tls.ai|api-$host.tls.ai|g" $dockerfile && SuccesfulPrint "Modify docker files"
+echo "2" > /opt/sg.f ##flag if the script has been run
 
 echo "DONE!"
+echo "Please reboot your machine"
+}
+mount_storage(){
+	##TODO
+}
+SuccesfulPrint(){
+	echo "=================================================================="
+	echo "                    $1 ....{green}success{reset}                  "
+	echo "=================================================================="
 }
 
+
+
+
+
+
+##Main
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+if [ "$EUID" -ne 0 ]; then	
+	echo "Please run this script as root"
+	echo "Exiting..."
+	exit
+fi
 if [[ -f "/opt/sg.f" ]]; then
-	after_reboot
+	if [[-f "/home/user/docker-compose/1.20.0/docker-compose.yml"]]; then
+		before_reboot
+	else
+		echo "App not installed, please Install it and try again"
+		echo "Exiting..."
+		exit
 else
 	before_reboot
 fi
