a1c8fb2e3b8415a3f7f0d0d251aac97d5dc9b518
scriptsandsuch <57178676+scriptsandsuch@users.noreply.github.com>
2019-11-13 11:31:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ else
 	exit
 fi
 ##Download files and install what is needed
-    read -p "Do you wish to install the enviroment automatically? [y/n]" yn
-    case $yn in
-        [Y|y]* ) install_env; break;;
+read -p "Do you wish to install the enviroment automatically? [y/n]" yn
+case $yn in
+	[Y|y]* ) install_env; break;;
         [N|n]* ) wget https://s3.eu-central-1.amazonaws.com/airgap.anyvision.co/better_environment/betterenvironment-181202-142-linux-x64-installer.run; 
 		break;;
 	* ) echo "${red}Invalid Answer${reset}"; exit;;
